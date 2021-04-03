8378786069a6967c43db0ad51a922ac6550a9b9e
Milacamila616 <56324531+Milacamila616@users.noreply.github.com>
2021-04-03 06:44:55 -0300

    Update media.c
    inclui o define

--- media.c
+++ media.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#define divisor 2
 
 int main (){
     int primeiro_numero=6, segundo_numero=18, soma_numeros, divide_numeros;
     soma_numeros = primeiro_numero+segundo_numero;
-    divide_numeros=soma_numeros/2;
+    divide_numeros=soma_numeros/divisor;    
 
-    printf("A media aritimetica entre %d e %d e:%d", primeiro_numero,segundo_numero,divide_numeros);
+printf("A media aritimetica entre %d e %d e:%d", primeiro_numero,segundo_numero,divide_numeros);
     return 0;   
 
 
