f46ac18eef715fbc16decde3f99fbc7fe3bc5e9a
Milacamila616 <56324531+Milacamila616@users.noreply.github.com>
2021-04-09 05:59:22 -0300

    Add files via upload

---
+++ exercicio_2a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+    //Declaração
+    float numero_1 = 2.3, numero_2 = 4.5, resultado;  
+
+    // Calculo
+    resultado = numero_1 + numero_2;
+
+
+    //Exibe
+    printf("O numero %.1f somado a %.1f tem como resultado: %.1f    ", numero_1, numero_2, resultado);
+    return 0;   
+
+
+
+
+}
