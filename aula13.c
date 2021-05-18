acbd0046cb59c367c6fb14877ba104b8f41212d5
Milacamila616 <56324531+Milacamila616@users.noreply.github.com>
2021-05-17 23:58:53 -0300

    Aula de C - Aprendendo esvaziar o buffer
    https://web.microsoftstream.com/video/abcd7156-faeb-421a-973d-12373af9936a 
    Aula de C

--- aula13.c
+++ aula13.c
@@ -3,7 +3,8 @@
 int main () {
     int a, b;
     float x, y;
-    char l1, l2;
+    char l1, l2; 
+    char palavra[12];
 
     printf ("Digite um numero inteiro: ");
     scanf ("%d", &a);
@@ -13,7 +14,11 @@ int main () {
     scanf("%d%f", &b, &x);
 
     printf("\n digite  uma letrinha: ");
-    scanf('%c', &l1);
+    scanf(' %c', &l1); //falta meche ai 42:12
 
+
+
+    scanf("\ndigite seu nome: ");
+    printf ("\npalavra: %s\n", palavra);
     return 0;
 }   
